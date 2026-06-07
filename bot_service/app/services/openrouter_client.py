320c387ac5aa3017462ba50ef68d866f1fa6ad62
Иван Кириян <ivankirian@MacBook-Air-Ivan-2.local>
2026-06-07 13:57:55 +0300

    Реализовано обращение к внешнему клиенту

---
+++ bot_service/app/services/openrouter_client.py
@@ -0,0 +1,30 @@
+import httpx
+
+from app.core.config import settings
+
+async def connection_openrouter(prompt:str) -> str: # подклбчение к клиенту
+    headers = {
+        "Authorization": f"Bearer {settings.OPENROUTER_API_KEY}",
+        "HTTP-Referer": settings.OPENROUTER_SITE_URL,
+        "X-Title": settings.OPENROUTER_APP_NAME,
+        "Content-Type": "application/json",
+    }
+    payload = {
+        "model": settings.OPENROUTER_MODEL,
+        "messages": [{"role": "user", "content": prompt}],
+    }
+
+    async with httpx.AsyncClient(timeout=60.0) as client:
+        response = await client.post(
+            f"{settings.OPENROUTER_BASE_URL}/chat/completions",
+            headers=headers,
+            json=payload,
+        )
+    
+    if response.status_code != 200:
+        raise RuntimeError(
+            f"Ошибка клиента {response.status_code}: {response.text}"
+        )
+    
+    data = response.json()
+    return data["choices"][0]["message"]["content"]
